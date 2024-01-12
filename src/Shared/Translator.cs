4eb452147aae1b44dfc3ca2e7ec8cce8a927b4c9
Yuta Matsumura <tkmbmbr@gmail.com>
2024-01-12 09:15:54 +0900

    create Translator.cs

---
+++ src/Shared/Translator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CognitiveServices.Speech;
+
+namespace Shared;
+
+public class Translator
+{
+    private readonly SpeechTranslationConfig _speechTranslationConfig;
+
+    public Translator(Uri endpointUrl, string subscriptionKey, string recognitionLanguage = "en-US", string targetLanguage = "ja-JP")
+    {
+        if (endpointUrl is null)
+        {
+            throw new ArgumentNullException(nameof(endpointUrl));
+        }
+
+        if (string.IsNullOrEmpty(subscriptionKey))
+        {
+            throw new ArgumentException($"'{nameof(subscriptionKey)}' を NULL または空にすることはできません。", nameof(subscriptionKey));
+        }
+
+        if (string.IsNullOrEmpty(recognitionLanguage))
+        {
+            throw new ArgumentException($"'{nameof(recognitionLanguage)}' を NULL または空にすることはできません。", nameof(recognitionLanguage));
+        }
+
+        if (string.IsNullOrEmpty(targetLanguage))
+        {
+            throw new ArgumentException($"'{nameof(targetLanguage)}' を NULL または空にすることはできません。", nameof(targetLanguage));
+        }
+
+        _speechTranslationConfig = SpeechTranslationConfig.FromEndpoint(endpointUrl, subscriptionKey);
+        _speechTranslationConfig.SpeechRecognitionLanguage = recognitionLanguage;
+        _speechTranslationConfig.AddTargetLanguage(targetLanguage);
+        _speechTranslationConfig.SetProperty(PropertyId.SpeechServiceConnection_TranslationVoice, "de-DE-Hedda");
+    }
+}
