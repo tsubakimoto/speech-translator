933046272492fa76dc60490ca48d7a3403d0853c
Yuta Matsumura <tkmbmbr@gmail.com>
2025-01-19 22:07:23 +0900

    enable appsettings.json

--- src/SpeechTranslatorDesktop/App.xaml.cs
+++ src/SpeechTranslatorDesktop/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
 using System.Windows;
+
+using Microsoft.Extensions.Configuration;
 
 namespace SpeechTranslatorDesktop
 {
@@ -9,6 +9,18 @@ namespace SpeechTranslatorDesktop
     /// </summary>
     public partial class App : Application
     {
-    }
+        public static IConfiguration Configuration { get; private set; }
 
+        public App()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT") ?? "Development";
+
+            // https://learn.microsoft.com/ja-jp/dotnet/desktop/wpf/migration/?view=netdesktop-8.0
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
 }
